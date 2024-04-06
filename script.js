a58460e5705320a13069ff9abc344cea7fc7de57
Rashikesh <rashikesh009@gmail.com>
2024-04-06 16:36:47 +0530

    page 3 & 4 complete

--- script.js
+++ script.js
@@ -72,5 +72,46 @@ rightElems.forEach(function(elem){
     
 })
 }
+
+function page3Animation(){
+
+var page3Center = document.querySelector(".page3-center");
+var video = document.querySelector("#page3 video");
+
+page3Center.addEventListener("click",function(){
+    video.play()
+    gsap.to(video,{
+        transform: "scaleX(1) scaleY(1)",
+        opacity: 1,
+        borderRadius: 0
+    })
+})
+
+video.addEventListener("click", function () {
+    video.pause()
+    gsap.to(video, {
+        transform: "scaleX(0.7) scaleY(0)",
+        opacity: 0,
+        borderRadius: "30px"
+    })
+})
+
+}
+
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave", function () {
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
+
+
 // navAnimation()
 page2Animation()
+page3Animation()
